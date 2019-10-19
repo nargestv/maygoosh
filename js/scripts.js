de38a8a59dc7991c4c34ca48bdd67faa6ffa13a3
narges <nargestv@gmail.com>
2019-10-19 16:38:21 +0330

    fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -51,30 +51,16 @@ function contactForm() {
 }
 
 $(document).ready(function(){
+
+
     $('.background-image-holder').each(function() {
         var imgSrc = $(this).children('img').attr('src');
         $(this).css('background', 'url("' + imgSrc + '")').css('background-position', 'initial').css('opacity','1');
     });
-  /*  Zepto(function($){
-        $(".main").onePageScroll({
-            loop: true
-        });
-    });*/
-  /*  Zepto(".main").onePageScroll({
-        sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-        easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-                                         // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-        animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-        pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-        updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-        beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-        afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-        loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-        keyboard: true,                  // You can activate the keyboard controls
-        responsiveFallback: false        // You can fallback to normal page scroll by defining the width of the browser in which
-                                         // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-                                         // the browser's width is less than 600, the fallback will kick in.
-    });*/
+
+    //result percentage
+
+
     var answers =[], answer,cnt=0;
    // result();
 /*    $('.main-carousel').flickity({
@@ -139,10 +125,12 @@ $(document).ready(function(){
     });
 
     $('.result-page-butt a').bind('click', function (e) {
-        console.log(answers);
+
             var lnt = answers.length;
             var result = Math.round((lnt / 3 )*100);
             $('.result-page').html(result + '%');
+            $(".progress-bar").data("percent",result);
+            $(".progress-bar").loading();
 
     });
 
